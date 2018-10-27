97c76ee8bc019743b1a7020c5bf91ae631da449c
KiraBurova <coca-cola-from-hell@yandex.ru>
2018-10-27 14:52:29 +0300

    add menu functionality

--- src/app.js
+++ src/app.js
@@ -1,1 +1,13 @@
 import './styles/main.scss';
+
+
+// Menu
+const menuBtn = document.querySelector('.header__lines');
+const menuNav = document.querySelector('.header__nav');
+
+function toggleMenu() {
+    menuNav.classList.toggle('open');
+}
+
+menuBtn.addEventListener('click', toggleMenu);
+

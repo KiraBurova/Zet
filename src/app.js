c2850516fbc5122b12a42cc29ec5b019deb807f0
KiraBurova <coca-cola-from-hell@yandex.ru>
2018-10-28 18:15:12 +0300

    fix arrow, remove dist

--- src/app.js
+++ src/app.js
@@ -5,7 +5,8 @@ import './styles/main.scss';
 // var glide = new Glide('.clients .glide', {
 //     type: 'carousel'
 //   })
-  
+
+
 //   glide.mount()
 
 // Menu
@@ -17,3 +18,18 @@ function toggleMenu() {
 }
 
 menuBtn.addEventListener('click', toggleMenu);
+
+let anchorlinks = document.querySelectorAll('a[href^="#"]');
+
+for(let item of anchorlinks) { // relitere
+    item.addEventListener('click', (e)=> {
+        let hashval = item.getAttribute('href');
+        let target = document.querySelector(hashval);
+        target.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+        history.pushState(null, null, hashval);
+        e.preventDefault();
+    });
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,65 +1,71 @@
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const SpritesmithPlugin = require('webpack-spritesmith');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
 var path = require('path');
 
 module.exports = {
-  mode: 'development',
-  entry: './src/app.js',
-  module: {
-    rules: [
-          {
-            test: /\.css$|\.sass$|\.scss$/,
-            use: [
-              MiniCssExtractPlugin.loader,
-              {
-                loader: 'css-loader?url=false'
-              },
-              { loader: 'sass-loader' },
-            ],
-          },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader'
-      },
-      {test: /\.png$|jpg|jpeg/, use: [
-        'file-loader?name=i/[hash].[ext]'
-    ]}
-    ],
-  },
-  resolve: {
-    extensions: ['.js', '.es6'],
-    modules: ['node_modules', 'spritesmith-generated']
-  },
-  output: {
-    path: __dirname + '/dist',
-    filename: 'index.js',
-  },
-  devServer: {
-    port: 8080,
-    contentBase: './src',
-    watchContentBase: true
- },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: "[name].css"
-    }),
-    new HtmlWebpackPlugin({
-        template: './src/index.html'
-    }),
-    new SpritesmithPlugin({
-      src: {
-          cwd: path.resolve(__dirname, 'src/assets/img/icons'),
-          glob: '*.png'
-      },
-      target: {
-          image: path.resolve(__dirname, 'src/spritesmith-generated/sprite.png'),
-          css: path.resolve(__dirname, 'src/spritesmith-generated/sprite.scss')
-      },
-      apiOptions: {
-          cssImageRef: '../spritesmith-generated/sprite.png'
-      }
-  })
-  ],
+    mode: 'development',
+    entry: './src/app.js',
+    module: {
+        rules: [
+            {
+                test: /\.css$|\.sass$|\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader?url=false'
+                    },
+                    { loader: 'sass-loader' }
+                ]
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },
+            {test: /\.png$|jpg|jpeg/, use: [
+                'file-loader?name=i/[hash].[ext]'
+            ]}
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.es6'],
+        modules: ['node_modules', 'spritesmith-generated'],
+        alias: {
+            'assets': path.resolve('src/assets/img')
+        }
+    },
+    output: {
+        path: __dirname + '/dist',
+        filename: 'index.js'
+    },
+    devServer: {
+        port: 8080,
+        contentBase: './src',
+        watchContentBase: true
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '[name].css'
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+        new SpritesmithPlugin({
+            src: {
+                cwd: path.resolve(__dirname, 'src/assets/img/icons'),
+                glob: '*.png'
+            },
+            target: {
+                image: path.resolve(__dirname, 'src/assets/img/sprite.png'),
+                css: path.resolve(__dirname, 'src/styles/sprite.scss')
+            },
+            apiOptions: {
+                cssImageRef: './assets/img/sprite.png'
+            }
+        }),
+        new CopyWebpackPlugin([{from: './src/assets', to: './assets'}])
+    ]
 };
